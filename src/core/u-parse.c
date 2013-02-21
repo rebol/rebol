rebol/rebol

commit 44012cc1e7313f2fd3772e543f2f8b9096d90f9c

fixed PARSE tag bug

src/core/u-parse.c
@@ -518,10 +518,11 @@ bad_target:
 					i = Find_Str_Str(series, 0, index, series->tail, 1, ser, 0, ser->tail, HAS_CASE(parse));
 					if (i != NOT_FOUND && is_thru) i += ser->tail;
 				}
-				else
+				else {
 					i = Find_Str_Str(series, 0, index, series->tail, 1, VAL_SERIES(item), VAL_INDEX(item), VAL_LEN(item), HAS_CASE(parse));
+					if (is_thru) i += VAL_LEN(item);
+				}
 				if (i >= series->tail) i = NOT_FOUND;
-				else if (is_thru) i += VAL_LEN(item);
 			}
 			// #"A"
 			else if (IS_CHAR(item)) {

commit eaf0d94bca2408f4adbf9993c77b388521d7a939

Fix PARSE regression: THRU not matching at the end
Commit 44012cc1e7313f2fd3772e543f2f8b9096d90f9c introduced a regression
that causes THRU rules to fail when it matches to the end of input. A
simple test case illustrating the regression:

    >> parse "abcd" [thru "d"]
    == false  ;; Expected: true

Internally, this is caused by a left-over check that was previously used
to handle not-found results from the internal Find_* helpers. This check
tests against past-or-on (>=) end position, which is now wrong, because
THRU positioning happens before the check occurs and it is perfectly
valid for THRU to advance _to_ end position. So if anything, we'd have
to relax that check to test against past (>) end position. But even that
is not needed, because this check is no longer necessary at all, as all
matching branches correctly check on their own for NOT_FOUND results.

There is one occurrence of the "old" pattern for testing against
NOT_FOUND left in the Parse_To function. This will be corrected for
consistency in a follow-up commit.

The regression was originally reported by Boleslav Brezovsky on AltME.

This fixes CureCode issue #1959.

src/core/u-parse.c
@@ -522,7 +522,6 @@ bad_target:
 					i = Find_Str_Str(series, 0, index, series->tail, 1, VAL_SERIES(item), VAL_INDEX(item), VAL_LEN(item), HAS_CASE(parse));
 					if (i != NOT_FOUND && is_thru) i += VAL_LEN(item);
 				}
-				if (i >= series->tail) i = NOT_FOUND;
 			}
 			// #"A"
 			else if (IS_CHAR(item)) {