42653da64ce29a2f2f15fc9438faa241323e44d5
Ladislav <lmecir@volny.cz>
2013-03-07 14:33:18 +0100

    Collect unused GOBs Resolving bug#1989. Define new GOB_MARK and GOB_USED GC flags. Use the resv field to store GC flags.

--- src/core/m-gc.c
+++ src/core/m-gc.c
@@ -115,7 +115,11 @@ static void Mark_Series(REBSER *series, REBCNT depth);
 	REBGOB **pane;
 	REBCNT i;
 
-	if (GOB_PANE(gob) && !IS_MARK_SERIES(GOB_PANE(gob))) {
+	if (IS_GOB_MARK(gob)) return;
+
+	MARK_GOB(gob);
+
+	if (GOB_PANE(gob)) {
 		MARK_SERIES(GOB_PANE(gob));
 		pane = GOB_HEAD(gob);
 		for (i = 0; i < GOB_TAIL(gob); i++, pane++) {
@@ -409,6 +413,48 @@ mark_obj:
 
 /***********************************************************************
 **
+*/	static REBCNT Sweep_Gobs(void)
+/*
+**		Free all unmarked gobs.
+**
+**		Scans all gobs in all segments that are part of the
+**		GOB_POOL. Free gobs that have not been marked.
+**
+***********************************************************************/
+{
+	REBSEG	*seg;
+	REBGOB	*gob;
+	REBCNT  n;
+	REBCNT	count = 0;
+
+	for (seg = Mem_Pools[GOB_POOL].segs; seg; seg = seg->next) {
+		gob = (REBGOB *) (seg + 1);
+		for (n = Mem_Pools[GOB_POOL].units; n > 0; n--) {
+#ifdef MUNGWALL
+			gob = (gob *) (((REBYTE *)s)+MUNG_SIZE);
+			MUNG_CHECK(GOB_POOL, gob, sizeof(*gob));
+#endif
+			if (IS_GOB_USED(gob)) {
+				if (IS_GOB_MARK(gob))
+					UNMARK_GOB(gob);
+				else {
+					Free_Gob(gob);
+					count++;
+				}
+			}
+			gob++;
+#ifdef MUNGWALL
+			gob = (gob *) (((REBYTE *)s)+MUNG_SIZE);
+#endif
+		}
+	}
+
+	return count;
+}
+
+
+/***********************************************************************
+**
 */	REBCNT Recycle(void)
 /*
 **		Recycle memory no longer needed.
@@ -475,6 +521,7 @@ mark_obj:
 	Mark_Series(Task_Series, 0);
 
 	count = Sweep_Series();
+	count += Sweep_Gobs();
 
 	CHECK_MEMORY(4);
 
