cbc747db844e291e80e1d206b7200408cd81342a
Marc Simpson <marc@0branch.com>
2013-03-10 23:09:50 +0000

    Implement BROWSE on Mac OS X.
    On OS X, `OS_Browse` only attempts to use `/usr/bin/open`; on other
    POSIX platforms, "xdg-open" and "x-www-browser" are used as before.

--- src/os/posix/host-lib.c
+++ src/os/posix/host-lib.c
@@ -615,8 +615,14 @@ static int Try_Browser(char *browser, REBCHR *url)
 /*
 ***********************************************************************/
 {
-	if (Try_Browser("xdg-open", url) || Try_Browser("x-www-browser", url))
-		return TRUE;
+	if (
+#if defined(TO_OSX) || defined(TO_OSXI)
+		Try_Browser("/usr/bin/open", url)
+#else
+		Try_Browser("xdg-open", url)
+		|| Try_Browser("x-www-browser", url)
+#endif
+	) return TRUE;
 	return FALSE;
 }
 
