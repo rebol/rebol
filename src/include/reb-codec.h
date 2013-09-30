7ae3e7aa3559c46e74a0af0d4b12834c165f2a40
Hostile Fork <brian@hostilefork.com>
2013-09-30 11:30:13 -0400

    Free intermediate buffers used by do-codec (CC #2068)

--- src/include/reb-codec.h
+++ src/include/reb-codec.h
@@ -29,6 +29,17 @@
 #define CODI_DEFINED
 
 // Codec image interface:
+//
+// If your codec routine returns CODI_IMAGE, it is expected that the
+// ->bits field contains a block of memory allocated with Make_Mem
+// of size (->w * ->h * 4).  This will be freed by the
+// REBNATIVE(do_codec) in n-system.c
+//
+// If your codec routine returns CODI_BINARY or CODI_TEXT, it is
+// expected that the ->data field contains a block of memory
+// allocated with Make_Mem of size ->len.  This will be freed by
+// the REBNATIVE(do_codec) in n-system.c
+//
 typedef struct reb_codec_image {
 	int action;
 	int w;
