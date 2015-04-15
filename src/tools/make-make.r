rebol/rebol

commit a99d2d6800207292e9590d8b1ea3d3c2c31bb8e2

Provide more info if make fails due to no local Rebol build tool.

make/makefile
@@ -1,4 +1,4 @@
-# REBOL Makefile -- Generated by make-make.r (do not edit) on 5-Feb-2013/3:25:06+1:00
+# REBOL Makefile -- Generated by make-make.r (do not edit) on 17-Aug-2013/20:18:59-7:00
 # This makefile is intentional kept simple to make builds possible on
 # a wider range of target platforms.
 
@@ -49,8 +49,9 @@ RFLAGS= -c -D$(TO_OS) -DREB_API  $(RAPI_FLAGS) $I
 HFLAGS= -c -D$(TO_OS) -DREB_CORE $(HOST_FLAGS) $I
 CLIB=  -ldl -m32 -lm
 
-# REBOL builds various include files:
-REBOL=	$(CD)r3-make -qs
+# REBOL is needed to build various include files:
+REBOL_TOOL= r3-make
+REBOL=	$(CD)$(REBOL_TOOL) -qs
 
 # For running tests, ship, build, etc.
 R3=	$(CD)r3$(BIN_SUFFIX) -qs
@@ -82,6 +83,21 @@ prep:
 	$(REBOL) $T/make-os-ext.r # ok, but not always
 	$(REBOL) $T/make-host-ext.r
 	$(REBOL) $T/make-reb-lib.r
+
+### Provide more info if make fails due to no local Rebol build tool:
+tmps: $S/include/tmp-bootdefs.h
+
+$S/include/tmp-bootdefs.h: $(REBOL_TOOL)
+	$(MAKE) prep
+
+$(REBOL_TOOL):
+	@echo
+	@echo "*** ERROR: Missing $(REBOL_TOOL) to build various tmp files."
+	@echo "*** Download Rebol 3 and copy it here as $(REBOL_TOOL), then"
+	@echo "*** make prep. Or, make prep on some other machine and copy"
+	@echo "*** the src/include files here. See README for details."
+	@echo
+	false
 
 ### Post build actions
 purge:
@@ -142,7 +158,7 @@ HOST =	objs/host-main.o objs/host-args.o objs/host-device.o objs/host-stdio.o \
 
 
 # Directly linked r3 executable:
-r3$(BIN_SUFFIX):	objs $(OBJS) $(HOST)
+r3$(BIN_SUFFIX):	tmps objs $(OBJS) $(HOST)
 	$(CC) -o r3$(BIN_SUFFIX) $(OBJS) $(HOST) $(CLIB)
 	$(STRIP) r3$(BIN_SUFFIX)
 	-$(NM) -a r3$(BIN_SUFFIX)

src/tools/make-make.r
@@ -83,8 +83,9 @@ RFLAGS= -c -D$(TO_OS) -DREB_API  $(RAPI_FLAGS) $I
 HFLAGS= -c -D$(TO_OS) -DREB_CORE $(HOST_FLAGS) $I
 CLIB=
 
-# REBOL builds various include files:
-REBOL=	$(CD)r3-make -qs
+# REBOL is needed to build various include files:
+REBOL_TOOL= r3-make
+REBOL=	$(CD)$(REBOL_TOOL) -qs
 
 # For running tests, ship, build, etc.
 R3=	$(CD)r3$(BIN_SUFFIX) -qs
@@ -116,6 +117,21 @@ prep:
 	$(REBOL) $T/make-os-ext.r # ok, but not always
 	$(REBOL) $T/make-host-ext.r
 	$(REBOL) $T/make-reb-lib.r
+
+### Provide more info if make fails due to no local Rebol build tool:
+tmps: $S/include/tmp-bootdefs.h
+
+$S/include/tmp-bootdefs.h: $(REBOL_TOOL)
+	$(MAKE) prep
+
+$(REBOL_TOOL):
+	@echo
+	@echo "*** ERROR: Missing $(REBOL_TOOL) to build various tmp files."
+	@echo "*** Download Rebol 3 and copy it here as $(REBOL_TOOL), then"
+	@echo "*** make prep. Or, make prep on some other machine and copy"
+	@echo "*** the src/include files here. See README for details."
+	@echo
+	false
 
 ### Post build actions
 purge:
@@ -152,7 +168,7 @@ check:
 
 makefile-link: {
 # Directly linked r3 executable:
-r3$(BIN_SUFFIX):	objs $(OBJS) $(HOST)
+r3$(BIN_SUFFIX):	tmps objs $(OBJS) $(HOST)
 	$(CC) -o r3$(BIN_SUFFIX) $(OBJS) $(HOST) $(CLIB)
 	$(STRIP) r3$(BIN_SUFFIX)
 	-$(NM) -a r3$(BIN_SUFFIX)

commit cbfad27303900c8dffa26a96d32e7a8f8c9842bd

Fix "Cannot find REBOL= definition" for make-make.r on Win32 (0.3.1)
This fixes a minor issue introduced by a99d2d6: when running
`make-make.r` with a target OS ID of `0.3.1` (Win32), you'd get a
"Cannot find REBOL= definition" message.

This was due to the simple makefile definition replacement logic looking
for "REBOL" would stop on the newly introduced "REBOL_TOOL" definition,
print above error message and fail to proceed to and update the real
"REBOL=" definition.

Further, the generated makefile would check for and try to run `r3-make`
on Win32, instead of the arguably more customary `r3-make.exe`.

The bundled `0.4.4` makefile was re-generated to incorporate the change
introduced to make `r3-make.exe` work. For 0.4.4 itself, that change is
purely cosmetic.

make/makefile
@@ -1,4 +1,4 @@
-# REBOL Makefile -- Generated by make-make.r (do not edit) on 5-Oct-2013/20:20:10+2:00
+# REBOL Makefile -- Generated by make-make.r (do not edit) on 16-Apr-2015/0:00:15+2:00
 # This makefile is intentional kept simple to make builds possible on
 # a wider range of target platforms.
 
@@ -50,7 +50,7 @@ HFLAGS= -c -D$(TO_OS) -DREB_CORE $(HOST_FLAGS) $I
 CLIB=  -ldl -m32 -lm
 
 # REBOL is needed to build various include files:
-REBOL_TOOL= r3-make
+REBOL_TOOL= r3-make$(BIN_SUFFIX)
 REBOL=	$(CD)$(REBOL_TOOL) -qs
 
 # For running tests, ship, build, etc.

src/tools/make-make.r
@@ -84,7 +84,7 @@ HFLAGS= -c -D$(TO_OS) -DREB_CORE $(HOST_FLAGS) $I
 CLIB=
 
 # REBOL is needed to build various include files:
-REBOL_TOOL= r3-make
+REBOL_TOOL= r3-make$(BIN_SUFFIX)
 REBOL=	$(CD)$(REBOL_TOOL) -qs
 
 # For running tests, ship, build, etc.
@@ -297,8 +297,12 @@ macro+: func [
 	n: join newline name
 	value: form value
 	unless parse makefile-head [
-		thru n any space ["=" | "?="] to newline  ; over simplified
-		insert #" " insert value to end
+		any [
+			thru n opt [
+				any space ["=" | "?="] to newline
+				insert #" " insert value to end
+			]
+		]
 	][
 		print ajoin ["Cannot find " name "= definition"]
 	]