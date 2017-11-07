0395c7703ccc85580ba561396423d5d4e0bf289b
Oldes <oldes.huhuman@gmail.com>
2017-11-07 16:57:17 +0100

    FEAT: multi-line input support for console
    Rewritten version of commit 10070b44f6565dccc3c13f7769b630b60cf31ccc
    which was prepared by Andreas Bolka, based on the code proposed by @sqlab
    via AltME.
    
    There is also improved part of handling Windows subsystem and missing title resource text for build using mingw on Windows.

--- src/os/host-main.c
+++ src/os/host-main.c
@@ -61,14 +61,15 @@
 
 /**********************************************************************/
 
-#define PROMPT_STR "\x1B[1;31;49m>>\x1B[0m "
-#define RESULT_STR "\x1B[1;31;49m==\x1B[33m "
-
 REBARGS Main_Args;
 
+#define PROMPT_STR "\x1B[1;31;49m>>\x1B[1;33;49m "
+#define RESULT_STR "\x1B[32m==\x1B[1;32;49m "
+
 #ifdef TO_WINDOWS
+#define MAX_TITLE_LENGTH  1024
 HINSTANCE App_Instance = 0;
-WCHAR     App_Title[1024]; //will be filled later from the resources file
+WCHAR     App_Title[MAX_TITLE_LENGTH]; //will be filled later from the resources file
 #endif
 
 #ifndef REB_CORE
@@ -83,11 +84,125 @@ extern void Init_Ext_Test(void);	// see: host-ext-test.c
 
 // Host bare-bones stdio functs:
 extern void Open_StdIO(void);
+extern void Close_StdIO(void);
 extern void Put_Str(char *buf);
 extern REBYTE *Get_Str();
 
 void Host_Crash(REBYTE *reason) {
 	OS_Crash("REBOL Host Failure", reason);
+}
+
+void Host_Repl() {
+	REBOOL why_alert = TRUE;
+
+#define MAX_CONT_LEVEL 1024
+	REBYTE cont_str[] = "\x1B[1;31;49m  \x1B[1;33;49m ";
+	int cont_level = 0;
+	REBYTE cont_stack[MAX_CONT_LEVEL] = { 0 };
+
+	int input_max = 32768;
+	int input_len = 0;
+	REBYTE *input = OS_Make(input_max);
+
+	REBYTE *line;
+	int line_len;
+
+	REBYTE *utf8byte;
+	BOOL inside_short_str = FALSE;
+	int long_str_level = 0;
+
+	while (TRUE) {
+		if (cont_level > 0) {
+			cont_str[11] = cont_level <= MAX_CONT_LEVEL ? cont_stack[cont_level - 1] : '-';
+			Put_Str(cont_str);
+		} else {
+			Put_Str(PROMPT_STR);
+		}
+
+		line = Get_Str();
+
+		if (!line) {
+			// "end of stream" - for example on CTRL+C
+			Put_Str("\x1B[0m"); //reset console color before leaving
+			goto cleanup_and_return;
+		}
+
+		line_len = 0;
+		for (utf8byte = line; *utf8byte; utf8byte++) {
+			line_len++;
+			switch (*utf8byte) {
+			case '^':
+				if (*(utf8byte + 1) != 0) {
+					line_len++;
+					utf8byte++;
+				}
+				break;
+			case '"':
+				if (long_str_level == 0) inside_short_str = !inside_short_str;
+				break;
+			case '[':
+			case '(':
+				if (!inside_short_str && long_str_level == 0) {
+					if (cont_level < MAX_CONT_LEVEL) cont_stack[cont_level] = *utf8byte;
+					cont_level++;
+				}
+				break;
+			case ']':
+			case ')':
+				if (!inside_short_str && long_str_level == 0) {
+					cont_level--;
+				}
+				break;
+			case '{':
+				if (!inside_short_str) {
+					if (cont_level < MAX_CONT_LEVEL) cont_stack[cont_level] = *utf8byte;
+					cont_level++;
+					long_str_level++;
+				}
+				break;
+			case '}':
+				if (!inside_short_str) {
+					cont_level--;
+					if (long_str_level > 0) long_str_level--;
+				}
+				break;
+			}
+		}
+		inside_short_str = FALSE;
+
+		if (input_len + line_len > input_max) {
+			REBYTE *tmp = OS_Make(2 * input_max);
+			if (!tmp) {
+				Put_Str("\x1B[0m"); //reset console color;
+				Host_Crash("Growing console input buffer failed!");
+			}
+			memcpy(tmp, input, input_len);
+			OS_Free(input);
+			input = tmp;
+			input_max *= 2;
+		}
+
+		memcpy(&input[input_len], line, line_len);
+		input_len += line_len;
+		input[input_len] = 0;
+
+		OS_Free(line);
+
+		if (cont_level > 0)
+			continue;
+
+		input_len = 0;
+		cont_level = 0;
+
+		Put_Str("\x1B[0m"); //reset color
+
+		RL_Do_String(input, 0, 0);
+		RL_Print_TOS(TRUE, RESULT_STR);
+	}
+
+cleanup_and_return:
+	OS_Free(input);
+	return;
 }
 
 
@@ -114,26 +229,26 @@ void Host_Crash(REBYTE *reason) {
 
 #ifdef TO_WINDOWS
 
-#ifdef WINDOWS_CONSOLE
+#ifdef _WINDOWS //use this define with Windows subsystem, by default use console subsystem
+int APIENTRY WinMain(HINSTANCE inst, HINSTANCE prior, LPSTR cmd, int show) {
+	int argc;
+	REBCHR **argv;
+	App_Instance = inst;
+#else
 int main(int argc, char **argv) {
 	// Retrieves the window handle used by the console associated with the calling process
 	HWND hwndC = GetConsoleWindow();
 	// Then we could just get the HINSTANCE:
 	App_Instance = GetModuleHandle(0); // HMODULE=HINSTANCE
-#else
-int APIENTRY WinMain(HINSTANCE inst, HINSTANCE prior, LPSTR cmd, int show) {
-	int argc;
-	REBCHR **argv;
+#endif
 	// Fetch the win32 unicoded program arguments:
 	argv = CommandLineToArgvW(GetCommandLineW(), &argc);
-	App_Instance = inst;
-#endif
-	LoadStringW(App_Instance, 101, App_Title, 100); //101 is hardcoded ID of application name string defined in resource file (.rc)
+	// Use title string as defined in resources file (.rc) with hardcoded ID 101
+	LoadStringW(App_Instance, 101, App_Title, MAX_TITLE_LENGTH);
 #else //non Windows platforms
 int main(int argc, char **argv) {
 #endif
 	REBYTE vers[8];
-	REBYTE *line;
 	REBINT n;
 
 	Host_Lib = &Host_Lib_Init;
@@ -180,29 +295,21 @@ int main(int argc, char **argv) {
 	n = RL_Start(0, 0, 0);
 #endif
 
-	// Console line input loop (just an example, can be improved):
 	if (
 		!(Main_Args.options & RO_CGI)
 		&& (
 			!Main_Args.script // no script was provided
 			|| n  < 0         // script halted or had error
-			|| Main_Args.options & RO_HALT  // --halt option
+			|| (Main_Args.options & RO_HALT)  // --halt option
 		)
 	){
-		n = 0;  // reset error code (but should be able to set it below too!)
-		while (TRUE) {
-			Put_Str(PROMPT_STR);
-			if ((line = Get_Str())) {
-				RL_Do_String(line, 0, 0);
-				RL_Print_TOS(0, RESULT_STR);
-				OS_Free(line);
-			}
-			else break; // EOS
-		}
+		Host_Repl();
 	}
 
 	//OS_Call_Device(RDI_STDIO, RDC_CLOSE);
 	OS_Quit_Devices(0);
+
+	Close_StdIO();
 
 	// A QUIT does not exit this way, so the only valid return code is zero.
 	return 0;
