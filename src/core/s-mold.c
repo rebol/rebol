f61b6a9349c0a5008071991b85b105251b09128b
Oldes <oldes.huhuman@gmail.com>
2017-11-07 16:54:25 +0100

    FEAT: errors printed in purple color

--- src/core/s-mold.c
+++ src/core/s-mold.c
@@ -957,7 +957,7 @@ STOID Mold_Error(REBVAL *value, REB_MOLD *mold, REBFLG molded)
 	err = VAL_ERR_VALUES(value);
 
 	// Form: ** <type> Error:
-	Emit(mold, "** WB", &err->type, RS_ERRS+0);
+	Emit(mold, "\x1B[1;35;49m** WB", &err->type, RS_ERRS+0);
 
 	// Append: error message ARG1, ARG2, etc.
 	msg = Find_Error_Info(err, 0);
