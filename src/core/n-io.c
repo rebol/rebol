5613dabea37cbb0d04d21177bd02586a5d5c8755
Marc Simpson <marc@0branch.com>
2018-04-14 18:54:06 -0700

    FIX: REBNATIVE(browse) should trap on OS_BROWSE failure, not success.

--- src/core/n-io.c
+++ src/core/n-io.c
@@ -513,11 +513,7 @@ chk_neg:
 
 	r = OS_BROWSE(url, 0);
 
-	if (r == 0) {
-		return R_UNSET;
-	} else {
-		Trap1(RE_CALL_FAIL, Make_OS_Error(r));
-	}
+	if (r == 0) Trap1(RE_CALL_FAIL, Make_OS_Error(r));
 
 	return R_UNSET;
 }
