13da594076618beddcbc0380174d43ef740d9872
Oldes <oldes.huhuman@gmail.com>
2022-01-20 15:36:42 +0100

    FIX: make sure that error message always starts on a new line
    resolves: https://github.com/Oldes/Rebol-issues/issues/2481

--- src/core/s-mold.c
+++ src/core/s-mold.c
@@ -1053,7 +1053,7 @@ STOID Mold_Error(REBVAL *value, REB_MOLD *mold, REBFLG molded)
 	err = VAL_ERR_VALUES(value);
 
 	// Form: ** <type> Error:
-	Emit(mold, "** WB", &err->type, RS_ERRS+0);
+	Emit(mold, "\n** WB", &err->type, RS_ERRS+0);
 
 	// Append: error message ARG1, ARG2, etc.
 	msg = Find_Error_Info(err, 0);
