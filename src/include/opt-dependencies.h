d70bab13b9257695718a4d108dde4c4052d5b63e
Oldes <oldes.huhuman@gmail.com>
2022-05-26 14:57:26 +0200

    FEAT: fine-tunning what crypto features are included in base/core/bulk builds

--- src/include/opt-dependencies.h
+++ src/include/opt-dependencies.h
@@ -49,19 +49,6 @@
 	#ifdef INCLUDE_RSA
 	#define MBEDTLS_RSA_C
 	#endif
-	#define MBEDTLS_BIGNUM_C
-	#define MBEDTLS_PKCS1_V15
-	#define MBEDTLS_OID_C
-	#define MBEDTLS_ENTROPY_C
-	#define MBEDTLS_AES_C
-	#define MBEDTLS_DHM_C
-	#define MBEDTLS_CTR_DRBG_C
-	#define MBEDTLS_ECDH_C
-	#define MBEDTLS_ECP_C
-	#define MBEDTLS_ECDSA_C
-	#define MBEDTLS_ASN1_PARSE_C
-	#define MBEDTLS_ASN1_WRITE_C
-
 
 	#define MBEDTLS_CIPHER_C
 
