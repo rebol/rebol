2ea23b1a25f076acaa3f0bdd662ffed58c869de6
Oldes Huhuman <oldes.huhuman@gmail.com>
2023-11-29 20:33:54 +0100

    FIX: ignore warning: ‘__builtin___strncpy_chk’ specified bound depends on the length of the source argument

--- src/os/posix/host-readline.c
+++ src/os/posix/host-readline.c
@@ -729,11 +729,14 @@ static struct termios Term_Attrs;	// Initial settings, restored on exit
 
 	// If we have leftovers:
 	if (term->residue[0]) {
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wstringop-overflow"
 		end = (int)LEN_BYTES(term->residue);
 		if (end < len) len = end;
 		COPY_STR(buf, term->residue, len); // terminated below
-		MOVE_MEM(term->residue, term->residue+len, end-len); // remove
+		COPY_MEM(term->residue, term->residue+len, end-len); // remove
 		term->residue[end-len] = 0;
+#pragma GCC diagnostic pop
 	}
 	else {
 		// Read next few bytes. We don't know how many may be waiting.
